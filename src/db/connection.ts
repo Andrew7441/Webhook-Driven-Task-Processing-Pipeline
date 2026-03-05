daede539bc0fc95ad2a912c4a13a417f38c7586f
Andrew7441 <andrewqumsieh12345@gmail.com>
2026-03-05 14:17:21 +0200

    Add connection improvements and env configuration

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,9 +1,12 @@
 import { Pool } from "pg"; // installed it first 
+import dotenv from "dotenv";
 
-export const pool = new Pool({ // connection manager for multiple dbs
-    host: "127.0.0.1", // connection to docker port mapping
-    port: 5432,
-    user: "postgres",
-    password: "postgres",
-    database: "postgres",
+dotenv.config(); // loads variables from .env
+
+export const pool = new Pool({          // connection manager for multiple dbs
+    host: process.env.DB_HOST,          // connection to docker port mapping
+    port: Number(process.env.DB_PORT),  // converted string to number
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 });
