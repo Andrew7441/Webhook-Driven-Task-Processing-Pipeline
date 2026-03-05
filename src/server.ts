2a0b9eb373c954a556e8daf0ed4c3dcb394aa32d
Andrew7441 <andrewqumsieh12345@gmail.com>
2026-03-05 15:42:53 +0200

    Add docker file and code improvements

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import { PipeLineRouter } from "./routes/pipelines";
+
 
 const app = express(); // create http server
 app.use(express.json()); // parses json req bodies, needed for webhooks
@@ -8,9 +10,13 @@ app.get("/health", (req, res) => {
   res.send({ status: "ok" });
 });
 
+// pipelines CRUD endpoints
+app.use("/pipelines", PipeLineRouter);
+
 const PORT = 8080;
 
 //starts the API server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
