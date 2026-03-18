8b5141100cba28c16b3229105fc8e9c9fe0de406
Andrew7441 <andrewqumsieh12345@gmail.com>
2026-03-18 20:03:39 +0200

    Add improvements

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import { SubscriberRouter } from "./routes/subscribers";
 import { testReceiverRouter } from "./routes/testReceiver";
 import { JobsRouter } from "./routes/jobs";
 import { metricsRouter } from "./routes/metrics";
+import { pool } from "./db/connection";
 
 const PORT = 8080;
 
@@ -13,8 +14,20 @@ app.use(express.json()); // parses json req bodies, needed for webhooks
 
 //simple endpoint to confirm server works 
 app.get("/health", (req, res) => {
-  res.send({ status: "ok" });
+  res.send({ Status: "Ok" });
 });
+
+// simple DB connectivity check
+app.get("/server", async (req, res) =>{
+  try{
+    const result = await pool.query(`SELECT 1`); // query to test DB
+    if(result.rowCount === 1) return res.send({ Status: "Ok", DataBase: "Ready"});
+
+    throw new Error("DB query failed");
+  }catch(err){
+    return res.status(503).send({ status: "error", db: "disconnected" });
+  }
+})
 
 // pipelines CRUD endpoints
 app.use("/pipelines", PipeLineRouter);
